1b5f9aca6d77cb58ceaa4c3452c4d27de5c6b7bd
CarlosGT7274 <carlosbt7274@gmail.com>
2023-04-22 12:40:40 -0600

    Update checkout.jsx

--- src/components/checkout.jsx
+++ src/components/checkout.jsx
@@ -1,11 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
-const checkout = () => {
+const Checkout = () => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    address: '',
+    city: '',
+    state: '',
+    zip: '',
+  });
+
+  const handleChange = e => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Container className="mt-4">
+      <Row>
+        <Col md={{ span: 6, offset: 3 }}>
+          <h3>Checkout</h3>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formFirstName">
+              <Form.Label>First Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter first name" name="firstName" value={formData.firstName} onChange={handleChange} required />
+            </Form.Group>
 
-export default checkout
+            <Form.Group controlId="formLastName">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter last name" name="lastName" value={formData.lastName} onChange={handleChange} required />
+            </Form.Group>
+
+            <Form.Group controlId="formAddress">
+              <Form.Label>Address</Form.Label>
+              <Form.Control type="text" placeholder="Enter address" name="address" value={formData.address} onChange={handleChange} required />
+            </Form.Group>
+
+            <Form.Row>
+              <Form.Group as={Col} controlId="formCity">
+                <Form.Label>City</Form.Label>
+                <Form.Control type="text" placeholder="Enter city" name="city" value={formData.city} onChange={handleChange} required />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formState">
+                <Form.Label>State</Form.Label>
+                <Form.Control type="text" placeholder="Enter state" name="state" value={formData.state} onChange={handleChange} required />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formZip">
+                <Form.Label>Zip</Form.Label>
+                <Form.Control type="text" placeholder="Enter zip" name="zip" value={formData.zip} onChange={handleChange} required />
+              </Form.Group>
+            </Form.Row>
+
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Checkout;
